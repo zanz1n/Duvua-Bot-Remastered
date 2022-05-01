659d4e111f32ab69d40b998200a2c9ad99cf37db
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-01 05:16:19 -0300

    added the config command to change server db

--- src/slashCommands/config/config.ts
+++ src/slashCommands/config/config.ts
@@ -1,12 +1,13 @@
 import slashCommand, { sInteraction } from '../../structures/slashCommand'
 import { MessageEmbed, MessageActionRow, MessageButton, Permissions } from 'discord.js'
 import Bot from '../../structures/Client'
+import guild from '../../database/models/guild'
 
 module.exports = class extends slashCommand {
     constructor(client: Bot) {
         super(client, {
             name: "config",
-            description: "Shows the bot ping and replies with pong",
+            description: "Altera configurações do bot no servidor",
             disabled: false,
             ephemeral: false,
             options: [
@@ -14,47 +15,99 @@ module.exports = class extends slashCommand {
                     type: 'SUB_COMMAND',
                     name: 'wellcome',
                     description: "Altera a mensagem de boas vindas do server",
-                    options: [
-                        {
-                            name: "canal",
-                            description: "O canal que você deseja usar as mensagens",
-                            type: 7,
-                            required: true
-                        },
-                        {
-                            name: "mensagem",
-                            description: "A mensagem de boas vindas que você deseja exibir",
-                            type: 3,
-                            required: true
-                        }
-                    ]
+                    options: [{
+                        name: "canal",
+                        description: "O canal que você deseja usar as mensagens",
+                        type: 7,
+                        required: true
+                    },
+                    {
+                        name: "mensagem",
+                        description: "A mensagem de boas vindas que você deseja exibir",
+                        type: 3,
+                        required: true
+                    }]
                 },
                 {
                     type: 'SUB_COMMAND',
                     name: 'prefix',
                     description: "Altera o prefixo do bot para o servidor",
-                    options: [
-                        {
-                            name: "prefixo",
-                            description: "O prefixo que deseja que o bot use",
-                            type: 3,
-                            required: true
-                        }
-                    ]
+                    options: [{
+                        name: "prefixo",
+                        description: "O prefixo que deseja que o bot use",
+                        type: 3,
+                        required: true
+                    }]
                 },
                 {
                     type: 'SUB_COMMAND',
                     name: 'disablewellcome',
-                    description: "Desabilita a mensagem de wellcome no server"
+                    description: "Desabilita a mensagem de bem-vindo no server"
+                },
+                {
+                    type: 'SUB_COMMAND',
+                    name: 'enablewellcome',
+                    description: "Habilita a mensagem de bem-vindo no server"
                 }
             ]
         })
     }
     run = async (interaction: sInteraction) => {
-        const button = new MessageActionRow().addComponents(
-            new MessageButton().setCustomId('yes').setLabel('Sim').setStyle('SUCCESS'),
-            new MessageButton().setCustomId('no').setLabel('No').setStyle('DANGER')
-        )
-        interaction.editReply({ components: [button] })
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+        const subCommand = interaction.options.getSubcommand()
+
+        if (!interaction.memberPermissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+            embed.setDescription(`**Você não tem permissão para usar esse comando, ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        const guilDb = await guild.findById(interaction.guild.id) ||
+            new guild({ _id: interaction.guild.id, name: interaction.guild.name })
+
+        await guilDb.save()
+        if (subCommand === "wellcome") {
+            const channel = interaction.options.getChannel("canal")
+            if (channel.type !== "GUILD_TEXT") {
+                embed.setDescription(`**Você precisa selecionar um canal de texto válido, ${interaction.user.username}**`)
+                return await interaction.editReply({ content: null, embeds: [embed] })
+            }
+            guilDb.wellcome = {
+                channel: channel.id,
+                message: interaction.options.getString('mensagem'),
+                enabled: true,
+            }
+            await guilDb.save()
+            embed.setDescription(`**Configurações salvas com sucesso, ${interaction.user.username}**`)
+            await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        else if (subCommand === "prefix") {
+            const prefix = interaction.options.getString("prefixo")
+
+            guilDb.prefix = prefix
+            guilDb.save()
+
+            embed.setDescription(`**Configurações salvas com sucesso, ${interaction.user.username}**\nAgora o novo prefixo é ${guilDb.prefix}`)
+            await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        else if (subCommand === "disablewellcome") {
+            guilDb.wellcome.enabled = false
+            guilDb.save()
+
+            embed.setDescription(`**Configurações salvas com sucesso, ${interaction.user.username}**\nAs mensagens de bem-vindo foram desabilitadas`)
+            await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        else if (subCommand === "enablewellcome") {
+            guilDb.wellcome.enabled = true
+            if (guilDb.wellcome.channel !== "na") {
+                embed.setDescription(`**Configurações salvas com sucesso, ${interaction.user.username}**
+            Caso deseje mudar a mensagem ou o canal use /config wellcome`)
+            } else {
+                guilDb.wellcome.channel = interaction.channel.id
+                embed.setDescription(`**Configurações salvas com sucesso, ${interaction.user.username}**
+            O canal da mensagem foi definido como esse.
+            Caso deseje mudar use /config wellcome`)
+            }
+            guilDb.save()
+            await interaction.editReply({ content: null, embeds: [embed] })
+        }
     }
 }
