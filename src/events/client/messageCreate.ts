13ea990248fa4c6e3fd5173f24c6bb82f4b1f8ab
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-01 05:15:06 -0300

    database updates on messaCreate event

--- src/events/client/messageCreate.ts
+++ src/events/client/messageCreate.ts
@@ -1,6 +1,7 @@
 import Event from "../../structures/Event"
 import Bot from "../../structures/Client"
 import { Message } from 'discord.js'
+import guild from '../../database/models/guild'
 
 module.exports = class extends Event {
     constructor(client: Bot) {
@@ -10,7 +11,17 @@ module.exports = class extends Event {
     }
     run = async (message: Message) => {
         if (message.author.bot) return
-        
+
+        const guilDb = await guild.findById(message.guild.id) ||
+            new guild({ _id: message.guild.id, name: message.guild.name })
+
+        await guilDb.save()
+
+        if (guilDb.name !== message.guild.name) {
+            guilDb.name = message.guild.name
+            await guilDb.save()
+        }
+
         if (this.client.config.dev_mode) console.log(`\x1b[36m[bot-events] Message created\x1b[0m`)
 
         if (message.content === `<@${this.client.user.id}>`) {
