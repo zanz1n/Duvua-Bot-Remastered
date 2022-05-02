0e44c4986ef3faeffe938c6cb2232acc01e956d8
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-02 00:03:14 -0300

    added adequate types

--- src/structures/slashCommand.ts
+++ src/structures/slashCommand.ts
@@ -1,4 +1,4 @@
-import { CacheType, Client, Interaction, Permissions, ThreadMember } from 'discord.js'
+import { CacheType, Client, Interaction, InteractionReplyOptions, InteractionDeferReplyOptions, InteractionDeferUpdateOptions } from 'discord.js'
 import Bot from '../structures/Client'
 
 class slashCommand {
@@ -22,16 +22,17 @@ class slashCommand {
 export default slashCommand
 
 export interface sInteraction extends Interaction {
-    reply: any
-    editReply: any
-    ticket: {
-        db: any
-    }
+    reply(options: InteractionReplyOptions)
+    deferReply(options: InteractionDeferReplyOptions)
+    deferUpdate(options: InteractionDeferUpdateOptions)
+    editReply(options: InteractionReplyOptions)
+    deleteReply: any
     customId: string
     options: {
         getString(name: string)
         getMember(name: string)
         getChannel(name: string)
+        getNumber(name: string)
         getSubcommand()
         getUser(name: string)
     }
