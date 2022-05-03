22064893fb0fb034dcb2fb3c4c3bc10cefba3b5e
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-03 00:18:47 -0300

    added say command

---
+++ src/slashCommands/utility/say.ts
@@ -0,0 +1,35 @@
+import slashCommand, { sInteraction } from "../../structures/slashCommand"
+import Bot from "../../structures/Client"
+import {
+    MessageEmbed,
+    Permissions
+} from 'discord.js'
+
+module.exports = class extends slashCommand {
+    constructor(client: Bot) {
+        super(client, {
+            name: "say",
+            description: "Eu falo o que você me pedir",
+            ephemeral: false,
+            disabled: false,
+            options: [
+                {
+                    name: "message",
+                    description: "O que você quer que eu fale",
+                    type: 3,
+                    required: true
+                }
+            ]
+        })
+    }
+    run = async (interaction: sInteraction) => {
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+        if (!interaction.memberPermissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
+            embed.setDescription(`**Você não tem permissão para usar esse comando,  ${interaction.user.username}**`)
+            return await interaction.editReply({ embeds: [embed] })
+        }
+
+        const content = interaction.options.getString('message') as string
+        await interaction.editReply({ content: `${content}\n-${interaction.user}` })
+    }
+}
