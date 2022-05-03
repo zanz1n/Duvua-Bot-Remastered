6cbeb931f11529c5a405160257b41bfe9913c892
zanz1n <izanrodrigues999@gmail.com>
2022-05-03 15:58:53 +0000

    added songinfo command

---
+++ src/slashCommands/music/songinfo.ts
@@ -0,0 +1,56 @@
+import slashCommand, { sInteraction } from "../../structures/slashCommand"
+import Bot from "../../structures/Client"
+import {
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton
+} from "discord.js"
+
+module.exports = class extends slashCommand {
+    constructor(client: Bot) {
+        super(client, {
+            name: "songinfo",
+            description: "Mostra o som que está tocando",
+            ephemeral: false,
+            disable: false,
+        })
+    }
+    run = async (interaction: sInteraction) => {
+        const queue = this.client.player.getQueue(interaction.guildId)
+
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+
+        if (!queue) {
+            embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+
+        let bar = queue.createProgressBar({
+            length: 19
+        })
+
+        const song = queue.current
+        const skip = new MessageButton()
+        .setCustomId('skip')
+        .setLabel('⏭️ Skip')
+        .setStyle('PRIMARY')
+        const stop = new MessageButton()
+        .setCustomId('stop')
+        .setLabel('⏹️ Stop')
+        .setStyle('DANGER')
+        const pause = new MessageButton()
+        .setCustomId('pause')
+        .setLabel('⏸️ Pause')
+        .setStyle('PRIMARY')
+        const resume = new MessageButton()
+        .setCustomId('resume')
+        .setLabel('▶️ Resume')
+        .setStyle('SUCCESS')
+
+        const button = new MessageActionRow().addComponents(skip, stop, pause, resume)
+
+        embed.setThumbnail(song.thumbnail).setDescription(`**${interaction.user}**\n\nTocando agora: [${song.title}](${song.url})\n\n**Duração: [${song.duration}]**\n\n ${bar}`)
+
+        await interaction.editReply({ content: null, embeds: [embed], components: [button] })
+    }
+}
