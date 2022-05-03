20df238e1f13936b700918233158001f74d6894d
zanz1n <izanrodrigues999@gmail.com>
2022-05-03 15:58:59 +0000

    added stop command

---
+++ src/slashCommands/music/stop.ts
@@ -0,0 +1,37 @@
+import slashCommand, { sInteraction } from "../../structures/slashCommand"
+import Bot from "../../structures/Client"
+import {
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton,
+    Permissions
+} from "discord.js"
+
+module.exports = class extends slashCommand {
+    constructor(client: Bot) {
+        super(client, {
+            name: "stop",
+            description: "Para o bot e limpa a fila de reprodução",
+            ephemeral: false,
+            disable: false,
+        })
+    }
+    run = async (interaction: sInteraction) => {
+        const queue = this.client.player.getQueue(interaction.guildId)
+
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+        if (!interaction.memberPermissions.has(Permissions.FLAGS.MOVE_MEMBERS)) {
+            embed.setDescription(`**Você não tem permissão para usar esse comando,  ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        if (!queue) {
+            embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+
+        queue.destroy()
+
+        embed.setDescription(`**A fila foi limpa por ${interaction.user}**`)
+        await interaction.editReply({ content: null, embeds: [embed] })
+    }
+}
