b9521254f6de1515dd338d83d36bb3c7028c5e64
zanz1n <izanrodrigues999@gmail.com>
2022-05-03 15:58:21 +0000

    added pause command

--- src/slashCommands/music/pause.ts
+++ src/slashCommands/music/pause.ts
@@ -17,5 +17,23 @@ module.exports = class extends slashCommand {
         })
     }
     run = async (interaction: sInteraction) => {
+        const queue = this.client.player.getQueue(interaction.guildId)
+
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+        if (!interaction.memberPermissions.has(Permissions.FLAGS.MOVE_MEMBERS)) {
+            embed.setDescription(`**Você não tem permissão para usar esse comando,  ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        if (!queue) {
+            embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+
+        queue.setPaused(true)
+
+        const resume = new MessageButton().setCustomId('resume').setLabel('▶️ Resume').setStyle('SUCCESS')
+        const button = new MessageActionRow().addComponents(resume)
+        embed.setDescription(`**Fila pausada por ${interaction.user}**\nUse /resume para continuar a reprodução`)
+        await interaction.editReply({ content: null, embeds: [embed], components: [button] })
     }
 }
