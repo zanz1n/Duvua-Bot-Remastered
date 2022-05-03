f0468a90fb3ceec6f4703f427ec0579a11e4428c
zanz1n <izanrodrigues999@gmail.com>
2022-05-03 15:58:44 +0000

    added skip command

---
+++ src/slashCommands/music/skip.ts
@@ -0,0 +1,30 @@
+import slashCommand, { sInteraction } from "../../structures/slashCommand"
+import Bot from "../../structures/Client"
+import {
+    MessageEmbed,
+} from "discord.js"
+
+module.exports = class extends slashCommand {
+    constructor(client: Bot) {
+        super(client, {
+            name: "skip",
+            description: "Pula a música que está tocando",
+            ephemeral: false,
+            disable: false,
+        })
+    }
+    run = async (interaction: sInteraction) => {
+        const queue = this.client.player.getQueue(interaction.guildId)
+
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+
+        if (!queue) {
+            embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+        queue.skip()
+
+        embed.setDescription(`**Música** ${queue.current.title} **pulada por ${interaction.user}**`)
+        await interaction.editReply({ content: null, embeds: [embed] })
+    }
+}
