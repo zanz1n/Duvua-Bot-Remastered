0289e8582c346a72c0af519515cec681a9a0b88c
zanz1n <izanrodrigues999@gmail.com>
2022-05-03 15:58:31 +0000

    added queue command

---
+++ src/slashCommands/music/queue.ts
@@ -0,0 +1,38 @@
+import slashCommand, { sInteraction } from "../../structures/slashCommand"
+import Bot from "../../structures/Client"
+import {
+    MessageEmbed,
+
+} from "discord.js"
+
+module.exports = class extends slashCommand {
+    constructor(client: Bot) {
+        super(client, {
+            name: "queue",
+            description: "Exibe o som que está tocando na fila",
+            ephemeral: false,
+            disable: false,
+        })
+    }
+    run = async (interaction: sInteraction) => {
+        const queue = this.client.player.getQueue(interaction.guildId)
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+
+        if (!queue || !queue.playing) {
+            embed.setDescription(`**Não há nenhum som na fila**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
+
+        const queueString = queue.tracks.join("\n")
+        const currentSong = queue.current
+
+        embed.setDescription("**Tocando**\n" +
+            (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : null) +
+            `\n\n**Lista**\n${queueString}`
+        )
+            .setFooter({ text: `Requisitado por ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() }).setTimestamp()
+            .setThumbnail(currentSong.thumbnail)
+
+        interaction.editReply({ content: null, embeds: [embed] })
+    }
+}
