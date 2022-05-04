b8f4cbe01db024b78e463f482442aa24b458d330
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-04 00:03:15 -0300

    adjusted work command cooldown

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema } from 'mongoose'
 let userSchema = new Schema({
     _id: { type: String },
     usertag: { type: String },
-    last_daily_request: { type: Number, default: new Date },
+    last_daily_request: { type: Number, default: Date.now() - 10800000 },
     job: { type: String, default: "mendigo" },
     gold_coins: { type: Number, default: 0 }
 })
