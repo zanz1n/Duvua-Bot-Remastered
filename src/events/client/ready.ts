4a33f11e545f029744119734ea132995cdadf518
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-04 00:03:58 -0300

    adjusting the log

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -18,7 +18,14 @@ module.exports = class extends Event {
             console.log("\x1b[31m[bot-err] Failed to connect to databse\x1b[0m")
             process.exit(1)
         })
+        function interGer(n: number) {
+            return n - n % 1
+        }
+        function yellow(str: string) {
+            return "\x1b[33m" + str + "\x1b[0m"
+        }
 
-        console.log(`\x1b[33m[bot-api] logged to discord-api as ${this.client.user.tag} in ${this.client.guilds.cache.size} guild(s)\x1b[0m`)
+        console.log(`${yellow("[bot-api]")} ${this.client.user.tag} ${yellow("|")} ${this.client.guilds.cache.size} guild(s) ${yellow("|")} ${this.client.config.dev_mode ? "development" : "production"} ${yellow("|")} ` +
+            `${interGer(process.memoryUsage().heapTotal / 1024 ** 2) + "/" + interGer(process.memoryUsage().rss / 1024 ** 2)} MB`)
     }
 }
