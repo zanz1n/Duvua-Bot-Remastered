1a6e3634c3ca75f5d0e8a8bd905e332ba6bc8b24
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-05 15:20:57 -0300

    can only skip if you have requested the music or have MANAGE_CHANNELS

--- src/slashCommands/music/skip.ts
+++ src/slashCommands/music/skip.ts
@@ -2,6 +2,7 @@ import slashCommand, { sInteraction } from "../../structures/slashCommand"
 import Bot from "../../structures/Client"
 import {
     MessageEmbed,
+    Permissions
 } from "discord.js"
 
 module.exports = class extends slashCommand {
@@ -22,9 +23,15 @@ module.exports = class extends slashCommand {
             embed.setDescription(`**Não há nenhum som na fila,  ${interaction.user}**`)
             return await interaction.editReply({ content: null, embeds: [embed] })
         }
-        queue.skip()
 
-        embed.setDescription(`**Música** ${queue.current.title} **pulada por ${interaction.user}**`)
-        await interaction.editReply({ content: null, embeds: [embed] })
+        if (interaction.memberPermissions.has(Permissions.FLAGS.MANAGE_CHANNELS) || interaction.user === queue.current.requestedBy) {
+            queue.skip()
+
+            embed.setDescription(`**Música** ${queue.current.title} **pulada por ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        } else {
+            embed.setDescription(`**Você não pode pular uma música que não solicitou, ${interaction.user}**`)
+            return await interaction.editReply({ content: null, embeds: [embed] })
+        }
     }
 }
