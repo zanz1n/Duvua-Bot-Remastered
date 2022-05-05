d127a8142504e3d89255574794b0006a9731e6df
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-05 15:17:09 -0300

    added queue legacycommand

---
+++ src/commands/music/queue.ts
@@ -0,0 +1,36 @@
+import Command, { sMessage } from '../../structures/Command'
+import Bot from '../../structures/Client'
+import {
+    MessageEmbed
+} from 'discord.js'
+
+module.exports = class extends Command {
+    constructor(client: Bot) {
+        super(client, {
+            name: "queue",
+            description: "Exibe o som que está tocando na fila",
+            disabled: false,
+            aliases: ['qu']
+        })
+    }
+    run = async (message: sMessage) => {
+        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+        const queue = this.client.player.getQueue(message.guild.id)
+
+        if (!queue) {
+            embed.setDescription(`**Não há nenhum som na fila, ${message.author}**`)
+            return await message.reply({ content: null, embeds: [embed] })
+        } else {
+            const queueString = queue.tracks.join("\n")
+            const currentSong = queue.current
+
+            embed.setDescription("**Tocando**\n" +
+                (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : null) +
+                `\n\n**Lista**\n${queueString}`)
+                .setFooter({ text: `Requisitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() }).setTimestamp()
+                .setThumbnail(currentSong.thumbnail)
+
+            await message.reply({ content: null, embeds: [embed] })
+        }
+    }
+}
