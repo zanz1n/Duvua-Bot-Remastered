39f09a7b627575db9e2bc45eb8268b8e49d772aa
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-08 23:27:03 -0300

    used the new help command pattern in the slashCmd help

--- src/slashCommands/info/help.ts
+++ src/slashCommands/info/help.ts
@@ -1,13 +1,11 @@
 import slashCommand, { sInteraction } from '../../structures/slashCommand'
 import {
-    MessageEmbed,
-    MessageActionRow,
-    MessageSelectMenu,
     SelectMenuInteraction,
     MessageComponentInteraction
 } from 'discord.js'
 import Bot from '../../structures/Client'
 import Guild from '../../database/models/guild'
+import replies from '../../utils/help'
 
 module.exports = class extends slashCommand {
     constructor(client: Bot) {
@@ -19,105 +17,43 @@ module.exports = class extends slashCommand {
         })
     }
     run = async (interaction: sInteraction) => {
+        const { guild, user, channel } = interaction
+
         const dateNow = new Date
-        const guilDb = await Guild.findById(interaction.guild.id) ||
-            new Guild({ _id: interaction.guild.id, name: interaction.guild.name })
+        const guilDb = await Guild.findById(guild.id) ||
+            new Guild({ _id: guild.id, name: guild.name })
 
-        const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
+        const filter = (btnInt: MessageComponentInteraction) => btnInt.isSelectMenu() && btnInt.user.id === user.id
 
-        const find = (command: string) => {
-            return this.client.slashCommands.find((c: slashCommand) => c.name === command).description ||
-                "Command not found"
-        }
+        const collector = channel.createMessageComponentCollector({ filter, time: 180000 })
 
-        embed.setTitle("Help")
-            .setDescription(`
-            **${this.client.user} pode fazer muitas coisas para o seu servidor.**
-            O que inclui tocar músicas, ver avatar de usuários, beijar alguém e até fazer um meme, confira todos os comandos do bot abaixo:\n
-            **\:globe_with_meridians: - slash commands | \:m: - legacy commands**\n
-            \:globe_with_meridians: - digite:  **/**  para ver as opções; \:m: - podem ser usados com o prefixo:  **${guilDb.prefix}** no chat`)
-
-            .addField("ℹ️ Categorias", "**Para visualizar todas as categorias e comandos use a caixa de seleção abaixo.**")
-
-            .setThumbnail(this.client.user.displayAvatarURL())
-
-            .setFooter({ text: `Requisitado por ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() }).setTimestamp()
-
-        const row = new MessageActionRow().addComponents(
-            new MessageSelectMenu().setCustomId('help').setPlaceholder('Escolha a categoria').setMinValues(1).setMaxValues(1)
-                .addOptions([
-                    { label: "Fun", value: `fun${dateNow}`, description: "Comandos para descontrair", emoji: "🥳", },
-                    { label: "Info", value: `info${dateNow}`, description: "Comandos para ver informações sobre o bot", emoji: "ℹ️" },
-                    { label: "Moderation / Utility", value: `mod-util${dateNow}`, description: "Comandos para auxiliar na moderação e organização do server", emoji: "🖋️" },
-                    { label: "Music", value: `music${dateNow}`, description: "Comandos para tocar musicas do youtube", emoji: "🎧" },
-                    { label: "Money / Level", value: `money${dateNow}`, description: "Comandos relacionados ao sistema monetário e de ranks do bot", emoji: "💸" },
-                ])
-        )
-
-        const filter = (btnInt: MessageComponentInteraction) => btnInt.isSelectMenu() && btnInt.user.id === interaction.user.id
-
-        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 180000 })
-
+        await interaction.editReply({
+            content: null,
+            embeds: [replies.index(this.client, user, guilDb)],
+            components: [replies.row(dateNow)]
+        })
         collector.on("collect", async (i: SelectMenuInteraction) => {
             const values = i.values[0]
             if (values === `fun${dateNow}`) {
-                const funEmbed = new MessageEmbed().setColor(this.client.config.embed_default_color)
-                    .addField("🥳 Fun", "Comandos for fun e de diversão em geral.")
-                    .addField("\:globe_with_meridians:\:m: kiss", find("kiss"), true).addField("\:globe_with_meridians:\:m: avatar", find("avatar"), true)
-                    .addField("\:m: bruno", "Não falamos do Bruno", true).addField("\:globe_with_meridians: meme", find("meme"), true)
-                    .addField("*", "*", true).addField("*", "*", true)
-
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [funEmbed] })
+                interaction.editReply({ content: null, embeds: [replies.fun(this.client, user, guilDb)] })
             }
             else if (values === `info${dateNow}`) {
-                const infoEmbed = new MessageEmbed().setColor(this.client.config.embed_default_color)
-                    .addField("ℹ️ Info", "Comandos de inforomação em geral.")
-                    .addField("\:globe_with_meridians:\:m: ping", find("ping"), true).addField("\:globe_with_meridians: info", find("info"), true)
-                    .addField("\:globe_with_meridians: help", find("help"), true).addField("\:globe_with_meridians:\:m: anime", find("anime"), true)
-                    .addField("\:globe_with_meridians: github", find("github"), true).addField("*", "*", true)
-
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [infoEmbed] })
+                interaction.editReply({ content: null, embeds: [replies.info(this.client, user, guilDb)] })
             }
             else if (values === `mod-util${dateNow}`) {
-                const modUtilEmbed = new MessageEmbed().setColor(this.client.config.embed_default_color)
-                    .addField("🖋️ Utilidade / Moderação", "Comandos para auxiliar na moderação e organização do server.")
-                    .addField("\:globe_with_meridians: embed", find("embed"), true).addField("\:globe_with_meridians: say", find("say"), true)
-                    .addField("\:globe_with_meridians: clear", find("clear"), true).addField("\:globe_with_meridians: ban", find("ban"), true)
-                    .addField("\:globe_with_meridians: config wellcome", "Altera a mensagem de boas vindas", true)
-                    .addField("\:globe_with_meridians: config prefix", "Altera o prefixo do bot no servidor", true)
-                //.addField("\:information_source: Dica", "**Nos comandos say e embed, use /n para quebrar linha.**")
-
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [modUtilEmbed] })
+                interaction.editReply({ content: null, embeds: [replies.modUtil(this.client, user, guilDb)] })
             }
             else if (values === `music${dateNow}`) {
-                const musicEmbed = new MessageEmbed().setColor(this.client.config.embed_default_color)
-                    .addField("🎧 Música", "Todos os comandos relacionados a músicas.")
-                    .addField("\:globe_with_meridians:\:m: play", find("play"), true).addField("\:globe_with_meridians:\:m: queue", find("queue"), true)
-                    .addField("\:globe_with_meridians: pause", find("pause"), true).addField("\:globe_with_meridians: resume", find("resume"), true)
-                    .addField("\:globe_with_meridians:\:m: skip", find("skip"), true).addField("\:globe_with_meridians: songinfo", find("songinfo"), true)
-                    .addField("\:globe_with_meridians:\:m: stop", find("stop"), true).addField("\:globe_with_meridians: dj add", "Adiciona um dj no server", true)
-                    .addField("\:globe_with_meridians: dj remove", "Remove algum dj do server", true)
-
-                    .addField("\:information_source: Dica", "**Dj podem controlar playlists e músicas sem que precisem de permissões adminitrativas no server.**")
-
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [musicEmbed] })
+                interaction.editReply({ content: null, embeds: [replies.music(this.client, user, guilDb)] })
             }
             else if (values === `money${dateNow}`) {
-                const moneyEmbed = new MessageEmbed().setColor(this.client.config.embed_default_color)
-                    .addField("💸 Dinheiro", "Todos os comandos relacionados ao sistema monetário.")
-                    .addField("\:globe_with_meridians: coins", find("coins"), true).addField("\:globe_with_meridians: work", find("work"), true)
-                    .addField("\:globe_with_meridians:\:m: level", find("level"), true).addField("\:globe_with_meridians: pay", find("pay"), true)
-                    .addField("*", "*", true).addField("*", "*", true)
-
                 i.deferUpdate()
-                interaction.editReply({ content: null, embeds: [moneyEmbed] })
+                interaction.editReply({ content: null, embeds: [replies.money(this.client, user, guilDb)] })
             }
-
         })
-        await interaction.editReply({ embeds: [embed], components: [row] })
     }
 }
