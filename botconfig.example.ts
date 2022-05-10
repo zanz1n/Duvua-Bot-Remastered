f1cb0c4bc75b76657c0566ebc667140a50877460
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-10 14:59:59 -0300

    no env variables

--- botconfig.example.ts
+++ botconfig.example.ts
@@ -12,14 +12,14 @@ export default class Config {
     log_all_loads: boolean
 
     constructor() {
-        this.token = "OTYwMzcwMTA5MDY1NTkyOTAy.YkpcXA.Ga0Jw2Zt7LbyFSdMN3ImAtzjq18"
-        this.serverid = "951236777560129627"
-        this.name = "Oyne Bot"
+        this.token = ""
+        this.serverid = ""
+        this.name = "Bot"
         this.embed_default_color = "#0000FF"
-        this.log_all_loads = true
         this.prefix = "-"
         this.dev_mode = true
+        this.log_all_loads = false
         this.cost = "R$: 10,99"
-        this.mongodb_url = "mongodb://duvuatest:Gankaku77@192.168.15.20:27017/test-dc-duvua-bot?authMechanism=DEFAULT&authSource=test-dc-duvua-bot"
+        this.mongodb_url = "mongodb://localhost:27017/test"
     }
 }
