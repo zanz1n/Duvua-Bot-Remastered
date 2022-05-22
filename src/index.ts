25d48b1a435c48fe4ce6162ad2b75ab72499d59b
Izan Rodrigues <98245141+zanz1n@users.noreply.github.com>
2022-05-22 04:39:49 -0300

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-console.log(`\x1b[33m[bot-api] Starting [...]\x1b[0m`)
-
 import { Bot } from './structures/Client'
+
+console.log(`\x1b[33m[bot-api] Starting [...]\x1b[0m`)
 
 const client = new Bot()
