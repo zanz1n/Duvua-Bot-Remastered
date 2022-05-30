81a9c95d5e9ffbfe496b0958a825acacbefe0f1b
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-05-30 00:34:51 -0300

    changed paths

--- src/events/commands/interactionCreate.ts
+++ src/events/commands/interactionCreate.ts
@@ -2,6 +2,7 @@ import { Event } from '../../structures/Event'
 import { Bot } from '../../structures/Client'
 import { sInteraction } from '../../types/Interaction'
 import { slashCommand } from "../../structures/slashCommand"
+import { Interaction } from 'discord.js'
 
 module.exports = class extends Event {
     constructor(client: Bot) {
@@ -9,10 +10,12 @@ module.exports = class extends Event {
             name: "interactionCreate"
         })
     }
-    run = async (interaction: sInteraction) => {
+    run = async (interaction: Interaction) => {
         if (interaction.user.bot) return
 
         if (interaction.isCommand()) {
+            if (interaction.replied || interaction.deferred) return
+
             const cmd = await this.client.slashCommands.find((c: slashCommand) => c.name === interaction.commandName)
 
             if (!cmd) return
@@ -30,7 +33,7 @@ module.exports = class extends Event {
                 )
             })
         }
-        if (interaction.isButton()) {
+        else if (interaction.isButton()) {
             if (interaction.customId === "permaTicketButton") {
                 await interaction.deferReply({ ephemeral: true })
                 const permaTicket = require('../../structures/handleEternalTickets')
@@ -41,9 +44,24 @@ module.exports = class extends Event {
                 if (!cmd) return
                 else {
                     await interaction.deferReply({})
-                    cmd.run(interaction)
+                    cmd.run(interaction).catch((err: Error) => {
+                        if (err) console.log("\x1b[31m[bot-err] something whent wrong trying to execute a buttonInteractionCommand\x1b[0m\n",
+                            err,
+                            "\n\x1b[33m[bot-api] this may affect the usability of the bot\x1b[0m"
+                        )
+                    })
                 }
             }
+        }
+        else if (interaction.isApplicationCommand()) {
+            const cmd = await this.client.slashCommands.find(cm => cm.name === interaction.commandName)
+            if (!cmd) return
+            else cmd.run(interaction).catch((err: Error) => {
+                if (err) console.log("\x1b[31m[bot-err] something whent wrong trying to execute a applicationCommand\x1b[0m\n",
+                    err,
+                    "\n\x1b[33m[bot-api] this may affect the usability of the bot\x1b[0m"
+                )
+            })
         }
     }
 }
