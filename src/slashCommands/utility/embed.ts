074b216956fb399c0e885e6327170f0990288cc2
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-06-12 00:26:04 -0300

    added \n line break to embed command

--- src/slashCommands/utility/embed.ts
+++ src/slashCommands/utility/embed.ts
@@ -3,6 +3,7 @@ import { sInteraction } from '../../types/Interaction'
 import { Bot } from '../../structures/Client'
 import { Embed as MessageEmbed } from '../../types/Embed'
 import {
+    Interaction,
     Permissions
 } from 'discord.js'
 
@@ -16,25 +17,29 @@ module.exports = class extends slashCommand {
             options: [
                 {
                     name: "message",
-                    description: "O que você quer que eu fale ~/n para quebrar linha~",
+                    description: "O que você quer que eu fale [\\n para quebrar linha]",
                     type: 3,
                     required: true
                 }
             ]
         })
     }
-    run = async (interaction: sInteraction) => {
+    run = async (interaction: Interaction) => {
+        if (!interaction.isCommand()) return
+        if (!interaction.deferred) return
+
         const embed = new MessageEmbed().setColor(this.client.config.embed_default_color)
         if (!interaction.memberPermissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
             embed.setDescription(`**Você não tem permissão para usar esse comando,  ${interaction.user.username}**`)
             return await interaction.editReply({ embeds: [embed] })
         }
-        if (interaction.options.getString('content') === "" || undefined) {
+        if (!interaction.options.getString("message")) {
             embed.setDescription("**Você precisa inserir algo na embed**")
             return await interaction.editReply({ embeds: [embed] })
         }
+        const content = interaction.options.getString("message").split('\\n').join('\n')
 
-        embed.setDescription(interaction.options.getString('content'))
+        embed.setDescription(content)
             .setFooter({ text: `Mensagem de ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() }).setTimestamp()
         await interaction.editReply({ embeds: [embed] })
     }
