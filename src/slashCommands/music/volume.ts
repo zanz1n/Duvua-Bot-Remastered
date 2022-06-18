6c8f6aafee90606f8fe7dfd336cf6a3d07ae2688
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-06-18 02:01:21 -0300

    added volume command

--- src/slashCommands/music/volume.ts
+++ src/slashCommands/music/volume.ts
@@ -1,8 +1,8 @@
 import { slashCommand } from "../../structures/slashCommand"
-import { Bot } from "../../structures/Client";
-import { Interaction, Permissions } from "discord.js";
-import { sInteraction } from "../../types/Interaction";
-import { Embed } from "../../types/Embed";
+import { Bot } from "../../structures/Client"
+import { MessageActionRow, MessageButton, Permissions } from "discord.js"
+import { sInteraction } from "../../types/Interaction"
+import { Embed } from "../../types/Embed"
 
 module.exports = class extends slashCommand {
     constructor(client: Bot) {
@@ -37,6 +37,8 @@ module.exports = class extends slashCommand {
     run = async (interaction: sInteraction) => {
         const subCommand = interaction.options.getSubcommand()
         const player = this.client.manager.get(interaction.guild.id)
+        const dateNow = new Date
+
         const embed = new Embed()
 
         if (!player || !player.queue.current) {
@@ -54,9 +56,62 @@ module.exports = class extends slashCommand {
                     embed.setDescription(`**O volume precisa ser um número entre 2 e 200, ${interaction.user}**`)
                     return interaction.editReply({ content: null, embeds: [embed] })
                 }
-                player.setVolume(parseInt((volume / 2).toString()))
-                embed.setDescription(`**O volume foi alterado para ${volume}% ${realVolume}**`)
-                return interaction.editReply({ content: null, embeds: [embed] })
+
+                if (volume > 100) {
+                    embed.setDescription(`**Um volume maior que 100% pode resultar em um aldio de baixa qualidade, deseja continuar ${interaction.user}?**`)
+
+                    const confirm = new MessageButton()
+                        .setCustomId(`yes-${dateNow}`)
+                        .setStyle('SUCCESS')
+                        .setLabel("Sim")
+                        .setEmoji(`✅`)
+                        .setDisabled(false)
+
+                    const cancel = new MessageButton()
+                        .setCustomId(`no-${dateNow}`)
+                        .setStyle('DANGER')
+                        .setLabel("Não")
+                        .setEmoji(`❌`)
+                        .setDisabled(false)
+
+                    const component = new MessageActionRow().addComponents(confirm, cancel)
+
+                    const filter = (i) => i.isButton() && i.user.id === interaction.user.id
+
+                    const collector = await interaction.channel.createMessageComponentCollector({
+                        filter,
+                        max: 1,
+                        time: 20000
+                    })
+
+                    await interaction.editReply({ content: null, embeds: [embed], components: [component] })
+
+                    collector.on("collect", async (i) => {
+                        const iEmbed = new Embed()
+
+                        if (i.customId === `yes-${dateNow}`) {
+                            player.setVolume(parseInt((volume / 2).toString()))
+                            iEmbed.setDescription(`**O volume foi alterado para ${volume}% ${realVolume}**`)
+
+                            confirm.setDisabled(true)
+                            cancel.setDisabled(true)
+
+                            i.deferUpdate()
+                            await interaction.editReply({ content: null, embeds: [iEmbed], components: [component] })
+                        } else {
+                            confirm.setDisabled(true)
+                            cancel.setDisabled(true)
+
+                            i.deferUpdate()
+                            await interaction.editReply({ components: [component] })
+                        }
+                    })
+                } else {
+                    player.setVolume(parseInt((volume / 2).toString()))
+                    embed.setDescription(`**O volume foi alterado para ${volume}% ${realVolume}**`)
+
+                    return interaction.editReply({ content: null, embeds: [embed] })
+                }
             } else {
                 embed.setDescription(`**Você não tem permissão para usar esse comando, ${interaction.user}**`)
                 return interaction.editReply({ content: null, embeds: [embed] })
